2941c0eb88aa5b8ba8885b4a1e397509608321c2
vypham <uty.tra@thebargainvillage.com>
2024-07-22 02:26:00 +0700

    message

--- ultis_get_input.py
+++ ultis_get_input.py
@@ -1,8 +1,13 @@
-# Remember to close the browser
 import os
 import pandas as pd
 import psycopg2
-from config import config,get_raw_data,trigger_github_workflow,insert_input_to_supabase,compare_data
+from config import (
+    config,
+    get_raw_data,
+    trigger_github_workflow,
+    insert_input_to_supabase,
+    compare_data,
+)
 
 # Get Selenium configuration
 chrome_options_list = config.get_selenium_config()
@@ -12,7 +17,7 @@ supabase = config.supabase
 
 db_config = config.get_database_config()
 current_time_gmt7 = config.current_time_gmt7
-GITHUB_TOKEN=os.getenv('MY_GITHUB_TOKEN')
+GITHUB_TOKEN = os.getenv("MY_GITHUB_TOKEN")
 
 
 # Your existing function to get the keyword from Supabase
@@ -30,34 +35,46 @@ def get_keyword_from_supabase():
         # Execute a query
         cur.execute(
             """
-                SELECT date,keyword_phrase FROM keyword_input_sourcing a where date=%s
-                """,
+                SELECT date, keyword_phrase FROM keyword_input_sourcing 
+                WHERE date = %s
+            """,
             (current_time_gmt7.strftime("%Y-%m-%d"),),
         )
         # Fetch all results
-        df = pd.DataFrame(cur.fetchall(), columns=["date","keyword_phrase"])
+        df = pd.DataFrame(cur.fetchall(), columns=["date", "keyword_phrase"])
         return df
-    except:
-        return []
+    except Exception as e:
+        print(f"Error fetching keywords from Supabase: {e}")
+        return pd.DataFrame()
     finally:
         if conn:
             conn.close()
 
+
 def get_data_from_supabase():
     response = supabase.table("keyword_input_sourcing").select("*").execute()
     if hasattr(response, "error") and response.error is not None:
-        raise Exception("Failed to fetch data from Supabase")  
+        raise Exception("Failed to fetch data from Supabase")
     else:
         return pd.DataFrame(response.data)
-while True:      
+
+
+while True:
     # Fetch raw data if needed
     supabase_data = get_data_from_supabase()
     if compare_data(get_raw_data(), supabase_data):
         print("New data found. Running workflow...")
-        insert_input_to_supabase(get_raw_data())
+        try:
+            insert_input_to_supabase(get_raw_data())
+        except Exception as e:
+            print(f"Error upserting data to Supabase: {e}")
+            continue
+
         # Fetch keyword from Supabase
         keywords = get_keyword_from_supabase()
-        if not keywords:
+        if keywords.empty:
             raise Exception("No keyword found for today's date")
-        for keyword in keywords:
+
+        for _, row in keywords.iterrows():
+            keyword = row["keyword_phrase"]
             trigger_github_workflow(keyword, GITHUB_TOKEN)
